e69a23c17fa2b39ce54e179cc6622d298c579d6c
Daniel Tridico <mail@danieltridico.com>
2019-03-14 15:40:48 +0200

    created new routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,19 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def about
+  end
+
+  def consulting
+  end
+
+  def careers
+  end
+
+  def contact
+  end
+
+  def impressum
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+
+  get 'about', to: 'pages#about'
+  get 'consulting', to: 'pages#consulting'
+  get 'careers', to: 'pages#careers'
+  get 'contact', to: 'pages#contact'
 end
